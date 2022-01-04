38318f4887261c94519d9a7441cf7ee1fa6f5a7b
Bảo Hưng <baohung2412@gmail.com>
2022-01-04 15:51:43 +0700

    change file type

---
+++ src/lib/components/Button/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/button.scss";
+
+const Button = (props) => {
+  const {
+    size = "s",
+    kind = "strong",
+    type = "primary",
+    label,
+    prefixIcon,
+    postfixIcon,
+  } = props;
+  return (
+    <div className={`sc-button__container ${size} ${kind} ${type}`}>
+      {prefixIcon && (
+        <img src={prefixIcon} className={`prefix-icon ${size}`} alt="" />
+      )}
+      <span>{label}</span>
+      {postfixIcon && (
+        <img src={postfixIcon} className={`postfix-icon ${size}`} alt="" />
+      )}
+    </div>
+  );
+};
+
+export default Button;
